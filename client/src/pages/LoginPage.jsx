8e47a9215166bfc88df2dc1a1721167ce7761057
Ishita-09-sahoo <ishita.sahoo0901@gmail.com>
2026-03-30 21:34:21 +0530

    signup and login page

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { motion as Motion } from "framer-motion";
+import Input from "../components/Input";
+import { Loader, Lock, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, isLoading] = useState(false);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    isLoading(true);
+    setTimeout(() => {
+      isLoading(false);
+    }, 500);
+  };
+
+  return (
+    <Motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800/50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-linear-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Welcome Back
+        </h2>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="flex items-center justify-center mb-6">
+            <Link
+              to={"/forgot-password"}
+              className="text-sm text-green-400 hover:underline"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+
+          <Motion.button
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            className="w-full py-3 px-4 bg-linear-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            type="submit"
+          >
+            {loading ? <Loader className="w-6 h-6 animate-spin mx-auto" /> : "Login"}
+          </Motion.button>
+        </form>
+      </div>
+
+      <div className="px-8 py-4 bg-gray-900/50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Don't have and account?{" "}
+          <Link to={"/signup"} className="text-green-400 hover:underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </Motion.div>
+  );
+}
+
+export default LoginPage;
